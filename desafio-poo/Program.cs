1e82bce1deba21b19d2e575950aee46adcb24de3
JFernando <almo_fernando@hotmail.com>
2023-12-21 20:02:27 -0300

    desafio de poo

---
+++ desafio-poo/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Celular Nokia:");
+Smartphone nokia = new Nokia(
+    numero:"123",
+    modelo:"modelo 1",
+    imei:"1",
+    memoria:128
+);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(
+    numero:"456",
+    modelo:"modelo 2",
+    imei:"2",
+    memoria:256
+);
+iphone.Ligar();
+iphone.InstalarAplicativo("Safari");
+ Console.WriteLine("\nPrograma concluído.");
